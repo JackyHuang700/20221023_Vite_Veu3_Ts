11f406452e0f05007f3efc559e2d1508ac7e93fd
jacky8595125@gmail.com <jacky8595125>
2022-10-30 20:42:47 +0800

    heroku test

--- src/composables/useApi/axiosService.ts
+++ src/composables/useApi/axiosService.ts
@@ -1,8 +1,9 @@
 import axios from "axios"
+import { /**AxiosRequestConfig, */ AxiosInstance, AxiosResponse, AxiosRequestConfig  } from 'axios'
 // import router from '@/router'
 
 // 创建一个axios实例
-const axiosService = axios.create({
+const axiosService:AxiosInstance = axios.create({
   // 设置请求接口的基地址
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   baseURL: "", // url = base url + request url
@@ -11,7 +12,7 @@ const axiosService = axios.create({
 })
 
 // 添加请求拦截器
-axiosService.interceptors.request.use(config => {
+axiosService.interceptors.request.use((config: AxiosRequestConfig) => {
   const _token = ""
   const _authorization = `Bearer ${_token}`
 
@@ -21,15 +22,30 @@ axiosService.interceptors.request.use(config => {
 }, error => Promise.reject(error))
 
 // 添加响应拦截器
-axiosService.interceptors.response.use(response => {
+axiosService.interceptors.response.use((response: AxiosResponse) => {
 
 
   // const { } = response.data
   console.log('interceptors.response 成功')
 
+
+
   return response
 
-}, error => Promise.reject(error))
+}, error => {
+
+  // https://juejin.cn/post/7070847898832863269?share_token=0ae4ec72-ee83-47cb-b7ee-226b73adf8fe
+
+
+  //    驗證失敗，登出
+  // if(){
+  //   router.push('/login')
+
+  //   Message.error('登录超时正在退出...')
+  // }
+
+  Promise.reject(error)
+})
 
 
 export default axiosService
